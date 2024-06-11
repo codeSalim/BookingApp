d53c1f3ba24f96419159699d69512b6f5413cfb1
codeSalim <abrarbitu@gmail.com>
2024-06-12 00:39:59 +0530

    mongoDb Connection done

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const dbconnect = require('./config/db')
 const app = express()
 require('dotenv').config()
 
+dbconnect()
 app.listen(process.env.PORT,()=>{
     console.log(`Server Is Runing On Port ${process.env.PORT}`);
 })
---
+++ Backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+const dbconnect = async()=>{
+    try {
+        const connection = await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MONGODB CONNECTION DONE !!!");
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+module.exports = dbconnect;
